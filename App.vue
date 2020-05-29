96187a0909bb61f732d1c67f20c79f6aade0890d
ADITYA KUMAR GUPTA <30798959+aditya1996gupta@users.noreply.github.com>
2020-05-29 21:27:15 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <message v-if="showMsg" msg="Vue Lifecycle Hooks"></message>
+    <button @click="showMsg = false">Hide</button>
+  </div>
+</template>
+
+<script>
+import Message from "./components/Message.vue";
+export default {
+  name: "app",
+  components: {
+    Message
+  },
+  data() {
+    return {
+      showMsg: true
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
