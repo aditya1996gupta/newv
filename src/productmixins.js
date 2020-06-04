55e22db37794fb3431b587e23ac668abbac09fa5
ADITYA KUMAR GUPTA <30798959+aditya1996gupta@users.noreply.github.com>
2020-06-04 20:53:22 +0530

    Add files via upload

---
+++ src/productmixins.js
@@ -0,0 +1,15 @@
+export default {
+  data () {
+    return {
+      products: []
+    }
+  },
+  created () {
+    this.products = this.loadProducts()
+  },
+  methods: {
+    loadProducts () {
+      return ['product a', 'product b', 'product c', 'product d', '  product e']
+    }
+  }
+}
